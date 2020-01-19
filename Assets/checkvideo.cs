af0ef93dc4936238b7b60a99ff5fb8b95fb34cc7
alexlo94 <alexlo@bu.edu>
2020-01-19 13:50:19 -0500

    Initial commit

---
+++ Assets/checkvideo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class checkvideo : MonoBehaviour {
+
+	public VideoPlayer vp;
+
+	// Use this for initialization
+	void Start () {
+		vp = gameObject.GetComponent<VideoPlayer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!vp.isPlaying){
+			Debug.Log("Video has stopped playing");
+			return;
+		}
+
+		Debug.Log("Video is playing");
+		
+	}
+}
